bd2af4ec6b36e846c382fe4c7b6d21b8f81cc02f
SebastianMunoz11 <sebastian_munoz11@live.com>
2019-05-07 16:00:02 -0500

    inicio

--- 01-http/02-servidor-web-nodejs/api-web/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {NestExpressApplication} from "@nestjs/platform-express";
+import {join} from "path";
+// import * as cookieParser from 'cookie-parser'
+const cookieParser = require('cookie-parser');
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory
+        .create(AppModule) as NestExpressApplication;
+    app.use(cookieParser('Secreto'));
+    app.setViewEngine('ejs');
+    app.setBaseViewsDir(join(__dirname, '..', 'views'));
+    app.use(express.static('publico'));
+    await app.listen(3000);
 }
+
+bootstrap();
 bootstrap();
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Mu-oz-Alvarez-Bryan-Sebastian
+# eguez-sarzosa-vicente-adrianjpMq
+Cambio 1.1.1
+Cambio 2
+Cambio 3
+Juanita 1
+Restar negativos
